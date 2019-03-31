cba15d87b643734aa00fb0d83004896d3b8809de
Michael Evans <mmevansjr@gmail.com>
2019-03-31 19:46:53 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -13,6 +13,12 @@ function whileLoop(num) {
 }
 
 function doWhileLoop(num) {
+  var i = 0;
+  function incrementVariable()
+  {
+    i = i + 1;
+    return i;
+  }
   do {
   console.log("I run once regardless.");
   } while (incrementVariable() < num);
